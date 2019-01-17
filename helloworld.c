22c91c34ff9dc42a3b4eb9f51e3eacef42278d1e
Jonathan Jay Powell <jpowell@cse.unl.edu>
2019-01-17 17:15:38 -0600

    Initial Commit

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jonathan Powell
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jon Powell, Major: Computer Science\n");
+
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Computer Science I
+## Hack 01 - Intro to Git & Github
+
+Github URL:  https://github.com/jjp601/UNL-CSCE155-hack1
+
+Author: Jon Powell
+Collaborators: 
